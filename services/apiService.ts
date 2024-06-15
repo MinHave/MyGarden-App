9c2d00b2d8d39f44b0849d787d6633861b00244c
Matias Grimm <37000378+MatiasGrimm@users.noreply.github.com>
2024-06-15 10:26:30 +0200

    Add missing functions from old apiservice file

--- services/apiService.ts
+++ services/apiService.ts
@@ -1,4 +1,12 @@
+import { ICurrentUser } from '@/store/interfaces/auth';
 import axios, { AxiosResponse } from 'axios';
+import { credentials, inOptions } from '@/types/apiService';
+import {
+  IGardenDetails,
+  IPlantDetails,
+  ISimpleGarden,
+  ISimplePlant,
+} from '@/types/interfaces';
 
 // Replace with your machine's IP address and port number
 const URL_ENDPOINT = 'https://api.tavsogmatias.com';
@@ -14,11 +22,6 @@ export interface ApiResponse<T = any> {
   success: boolean;
   message?: string;
   data?: T;
-}
-
-export interface Credentials {
-  username: string;
-  password: string;
 }
 
 const get = async <T = any>(url: string): Promise<ApiResponse<T>> => {
@@ -40,7 +43,8 @@ const get = async <T = any>(url: string): Promise<ApiResponse<T>> => {
 
 const post = async <T = any>(
   url: string,
-  data: any
+  data: object | string,
+  options?: inOptions
 ): Promise<ApiResponse<T>> => {
   try {
     console.log(`POST request to: ${url} with data:`, data);
@@ -56,6 +60,19 @@ const post = async <T = any>(
       message: error.response?.data?.message || error.message,
     };
   }
+};
+
+const postJsonString = async <T = any>(
+  url: string,
+  payload: object | string,
+  options: inOptions
+): Promise<ApiResponse<T>> => {
+  return post(url, JSON.stringify(payload), {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    ...options,
+  });
 };
 
 const put = async <T = any>(
@@ -95,6 +112,63 @@ const del = async <T = any>(url: string): Promise<ApiResponse<T>> => {
   }
 };
 
-export const login = async (credentials: Credentials): Promise<ApiResponse> => {
+//#region Auth
+export const login = async (
+  credentials: credentials
+): Promise<ApiResponse<ICurrentUser>> => {
   return post(`${URL_ENDPOINT}/auth/login`, credentials);
 };
+
+export const refreshAuth = async (
+  refreshCode: string
+): Promise<ApiResponse<ICurrentUser>> => {
+  return postJsonString('auth/refresh', refreshCode, { noAuth: true });
+};
+
+export const resetPassword = async (username: string) => {
+  return postJsonString('auth/resetPassword', username, {
+    noAuth: true,
+  });
+};
+//#endregion Auth
+
+//#region Getters
+export const getPlants = async (
+  gardenId: string | undefined
+): Promise<ApiResponse<ISimplePlant[]>> => {
+  return get(`plant/gardenPlants/${gardenId}`);
+};
+
+export const getPlant = async (
+  plantId: string
+): Promise<ApiResponse<ISimplePlant>> => {
+  return get(`plant/${plantId}`);
+};
+
+export const getGardenList = async (): Promise<
+  ApiResponse<ISimpleGarden[]>
+> => {
+  return get(`garden`);
+};
+
+export const getGarden = async (
+  gardenId: string
+): Promise<ApiResponse<ISimpleGarden>> => {
+  return get(`garden/${gardenId}`);
+};
+//#endregion Getters
+
+//#region Setters
+
+export const updatePlant = async (
+  plant: IPlantDetails
+): Promise<ApiResponse<IPlantDetails>> => {
+  return put(`plant`, plant);
+};
+export const updateGarden = async (
+  garden: IGardenDetails
+): Promise<ApiResponse<IPlantDetails>> => {
+  return put(`garden`, garden);
+};
+
+//#endregion Setters
